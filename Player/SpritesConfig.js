221d7caa6d98b5c9c05e3e5d2c11b79161492fcf
Daylahaoo <n20dcpt021@student.ptithcm.edu.vn>
2024-05-05 23:24:43 +0700

    Thêm hình ảnh cho người chơi

---
+++ Player/SpritesConfig.js
@@ -0,0 +1,9 @@
+const Sprites =[
+    {'name':'Bunny','idle':{path:'Image/Player/Bunny/idle.png',frame:4},'run':{path:'Image/Player/Bunny/run.png',frame:6},'flip':false,'size':{w:30,h:80}},
+    {'name':'FireDragon','idle':{path:'Image/Player/FireDragon/idle.png',frame:4},'run':{path:'Image/Player/FireDragon/run.png',frame:8},'flip':false,'size':{w:50,h:80}},
+    {'name':'Frog','idle':{path:'Image/Player/Frog/idle.png',frame:4},'run':{path:'Image/Player/Frog/run.png',frame:3},'flip':false,'size':{w:35,h:80}},
+    {'name':'GreenSlimer','idle':{path:'Image/Player/GreenSlimer/idle.png',frame:8},'run':{path:'Image/Player/GreenSlimer/run.png',frame:7},'flip':true,'size':{w:40,h:40}},
+    {'name':'VioletDragon','idle':{path:'Image/Player/VioletDragon/idle.png',frame:4},'run':{path:'Image/Player/VioletDragon/run.png',frame:6},'flip':false,'size':{w:20,h:20}},
+    {'name':'Vulture','idle':{path:'Image/Player/Vulture/idle.png',frame:4},'run':{path:'Image/Player/Vulture/run.png',frame:4},'flip':false,'size':{w:20,h:20}}
+]
+export default Sprites;
